46231dbd652c63c5df334f658e6cdb533faa476b
saraa93-bit <saraa93177228@gmail.com>
2023-12-24 10:05:42 +0200

    c

--- monty.h
+++ monty.h
@@ -1,14 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -16,27 +14,29 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 /**
  * struct args_s - structure of arguments from main
  * @av: name of the file from the command line
@@ -131,4 +131,5 @@ void free_everything(char **args);
 
 /* free.c */
 void free_all(int all);
+
 #endif
